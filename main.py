ea192e2b87e0be168ce1960968fa8552e9e4bad5
santistebanpilar-frm <maria.santisteban@alumnos.frm.utn.edu.ar>
2025-10-24 00:27:38 -0300

    creacion de main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+#Codigo principal con menu de opciones
+print("----------------------------------------------------------------------")
+print("---------- Bienvenido a la Base de Datos: Paises del Mundo -----------")
+print("-------------------------------- MENU --------------------------------")
+print("|1|Agregar país                                                       ")
+print("|2|Buscar país                                                        ")
+print("|3|Filtrar (Contiente|Superficie|Población)                           ")
+print("|4|Ordenar (Nombre|Población|Superfice                                ")
+print("|5|Estadisticas                                                       ")
+print("|6|Salir                                                              ")
+print("----------------------------------------------------------------------")
+
