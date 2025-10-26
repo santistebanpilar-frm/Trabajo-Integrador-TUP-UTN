1181ae42a3c3d9bc9b5938f31772019c2538a4a3
santistebanpilar-frm <maria.santisteban@alumnos.frm.utn.edu.ar>
2025-10-26 02:46:35 -0300

    modificacione en archivo main.py
    -creacion de funciones de las opciones del menu principal
    -funciones: buscar pais, leer csv, validar input

--- main.py
+++ main.py
@@ -45,16 +45,22 @@ def main():
             eleccion:int = funciones.menu()
             if eleccion == 1:
                 print("Opción 1 seleccionada.")
+                buscar_pais()
             elif eleccion == 2:
                 print("Opción 2 seleccionada.")
+                filtrar_paises()
             elif eleccion == 3:
                 print("Opción 3 seleccionada.")
+                ordenar_paises()
             elif eleccion == 4:
                 print("Opción 4 seleccionada.")
+                estadisticas()
             elif eleccion == 5:
                 print("Opción 5 seleccionada.")
+                agregar_pais()
             elif eleccion == 6:
                 print("Opción 6 seleccionada.")
+                actualizar_pais()
             elif eleccion == 7:
                 print("Saliendo del programa.")
             else:
@@ -63,6 +69,168 @@ def main():
             print(f"Programa terminado por el usuario.")
         except ValueError as e:
             print(f"Error de valor ingresado. Por favor, ingrese un número válido.")     
+
+#Funcnion para validar inputs del usuario
+def validar_entrada(texto):
+    if texto == "":
+       raise ValueError("No se permite dejar el campo vacío")
+    if texto.isspace():
+       raise ValueError("No se permite ingresar solo espacios en blanco")
+    if texto == ".":
+       raise ValueError("No se permite ingresar solo un punto")
+    return texto.title()
+
+#Funcion pra leer y verificar csv
+def leer_csv(path: str = 'paises.csv'):
+    try:
+        df = pd.read_csv(path)
+        return df
+    except FileNotFoundError:
+        print(f"Error: No se encontró el archivo '{path}'.")
+        return None
+    except pd.errors.EmptyDataError:
+        print(f"Error: El archivo '{path}' está vacío.")
+        return None
+
+#Funcion para buscar pais
+def buscar_pais():
+    # Leer el archivo CSV de forma segura
+    df = leer_csv()
+    if df is None:
+        return
+    busqueda_valida : bool = False
+    while not busqueda_valida:
+        try:
+        # Solicitar el nombre del país al usuario
+            entrada = input("Ingrese el nombre del país a buscar: ").strip()
+        # Validar la entrada (lanzará ValueError si no es válida)
+            pais_buscar = validar_entrada(entrada)    
+        # Buscar el país en el DataFrame
+            resultado = df[df['nombre'].str.contains(pais_buscar, case=False, na=False)]
+            busqueda_valida = True
+        except ValueError as e:
+            print(f"Error: {e}.Intente de nuevo.")
+        except KeyboardInterrupt:
+            print("Búsqueda cancelada por el usuario.")
+            return
+    #Mostrara todos los paises que se encuentren en la base de datos respecto a lo que ingreso el usuario
+    if len(resultado) > 0:
+        print("Pais encontrado: ")
+        for _,pais in resultado.iterrows():
+            print(f"Nombre: {pais['nombre']}") 
+            print(f"Poblacion: {pais['poblacion':,]}") 
+            print(f"Área: {pais['area_km2']:,.1f} km²") 
+            print(f"Contiente: {pais['continente']}") 
+    else:
+        print("El país ingresado no se encontra en la base de datos!")
+
+
+#Funcion para filtrar paises
+def filtrar_paises():    
+    print(f"""
+    ⏐⏐≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡⏐⏐
+    ⏐⏐                Filtrar paises               ⏐⏐
+    ⏐⏐≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡⏐⏐     
+    ⏐⏐           Seleccione una opción.            ⏐⏐
+    ⏐⏐                                             ⏐⏐ 
+    ⏐⏐          (1) Por Continentes.               ⏐⏐      
+    ⏐⏐          (2) Por Rango de Supercifie.       ⏐⏐      
+    ⏐⏐          (3) Por Rango de Población.        ⏐⏐
+    ⏐⏐          (4) Salir.                         ⏐⏐
+    ⏐⏐                                             ⏐⏐        
+    ⏐⏐≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡⏐⏐         
+        """)
+    #Solicitar al usuario que filtro desea realizar
+    opcion : str = str(input("Ingrese una opcion: "))
+    while opcion != "4":
+        if opcion == "1":
+            pass
+        elif opcion == "2":
+            pass
+        elif opcion == "3":
+            pass
+        elif opcion == "4":
+            print("Saliendo...")
+        else:
+            print("Opcion invalida, intente nuevamnte!")
+        
+
+#Funcion para ordenar paises
+def ordenar_paises():
+    print(f"""
+    ⏐⏐≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡⏐⏐
+    ⏐⏐                Ordenar paises               ⏐⏐
+    ⏐⏐≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡⏐⏐     
+    ⏐⏐           Seleccione una opción.            ⏐⏐
+    ⏐⏐                                             ⏐⏐ 
+    ⏐⏐          (1) Por Nombre.                    ⏐⏐      
+    ⏐⏐          (2) Por Poblacion.                 ⏐⏐      
+    ⏐⏐          (3) Por Superfice Ascendente.      ⏐⏐
+    ⏐⏐          (4) Por Superfice Desencente.      ⏐⏐
+    ⏐⏐          (5) Salir.                         ⏐⏐
+    ⏐⏐                                             ⏐⏐        
+    ⏐⏐≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡⏐⏐         
+        """)
+    #Solicitar al usuario que como ordenar a los paises
+    opcion : str = str(input("Ingrese una opcion: "))
+    while opcion != "4":
+        if opcion == "1":
+            pass
+        elif opcion == "2":
+            pass
+        elif opcion == "3":
+            pass
+        elif opcion == "4":
+            pass
+        elif opcion == "5":
+            print("Saliendo...")
+        else:
+            print("Opcion invalida, intente nuevamnte!")
+
+def estadisticas():
+    print(f"""
+    ⏐⏐≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡⏐⏐
+    ⏐⏐                 Estadisticas                ⏐⏐
+    ⏐⏐≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡⏐⏐     
+    ⏐⏐           Seleccione una opción.            ⏐⏐
+    ⏐⏐                                             ⏐⏐ 
+    ⏐⏐    (1) Pais con Mayor y Menor Población.    ⏐⏐      
+    ⏐⏐    (2) Promedio de Población.               ⏐⏐      
+    ⏐⏐    (3) Promedio de Superficie.              ⏐⏐
+    ⏐⏐    (4) Cantidad de Paises por Continente.   ⏐⏐
+    ⏐⏐    (5) Salir.                               ⏐⏐
+    ⏐⏐                                             ⏐⏐        
+    ⏐⏐≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡⏐⏐         
+        """)
+    #Solicitar al usuario que estadistica desea ver
+    opcion : str = str(input("Ingrese una opcion: "))
+    while opcion != "4":
+        if opcion == "1":
+            pass
+        elif opcion == "2":
+            pass
+        elif opcion == "3":
+            pass
+        elif opcion == "4":
+            pass
+        elif opcion == "5":
+            print("Saliendo...")
+        else:
+            print("Opcion invalida, intente nuevamnte!")
+
+#Funcion para agregar pais
+def agregar_pais():
+    # Leer el archivo CSV y verificar que exista
+    df = leer_csv()
+    if df is None:
+        return
+    
+#Funcion para actualizar pais
+def actualizar_pais():
+    # Leer el archivo CSV y verificar que exista
+    df = leer_csv()
+    if df is None:
+        return
 
 # Llamada a la función principal.
 if __name__ == '__main__':
