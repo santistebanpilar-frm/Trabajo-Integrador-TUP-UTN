578032dcbbb2819afd380fd469daebc8abef3289
Pablo F. Quirós Ricci <130782162+pebloricci@users.noreply.github.com>
2025-10-30 19:36:13 -0300

    actualización de versión
    Estamos arreglando un error.

--- funciones.py
+++ funciones.py
@@ -45,14 +45,12 @@ def leer_paises():
                         'continente': datos[3].strip()
                     }
                     paises.append(pais)
-            
-            return paises
-            
+            return paises      
     except FileNotFoundError:
         print("Error: no se encontró el archivo 'paises.csv'")
     except Exception as e:
         print(f"Error al leer los países: {str(e)}")
-    return []  # Retornar lista vacía en caso de error
+    return []  # Retornar lista vacía en caso de error.
 
 # Funciones de validaciones.
 def validar_input(texto):
@@ -71,20 +69,35 @@ def normalizar_input(texto):
   tabla = str.maketrans( "áéíóúüñ", "aeiouun" ) 
   return texto.translate(tabla)
 
+def elegir_continente(continente_a_buscar:int):
+    if continente_a_buscar == 1:
+        continente_elegido = 'África'
+    elif continente_a_buscar == 2:
+        continente_elegido = 'América'
+    elif continente_a_buscar == 3:
+        continente_elegido = 'Asia'
+    elif continente_a_buscar == 4:
+        continente_elegido = 'Europa'
+    elif continente_a_buscar == 5:
+        continente_elegido = 'Oceanía' 
+    elif continente_a_buscar == 6:
+        continente_elegido = 'Antártida'    
+    return continente_elegido
+
 # -------------------------------
 # Funciones principales del menú.
 # -------------------------------
 
 # Elección 1: Buscar país.
 def buscar_pais():
-    paises = leer_paises()
+    paises:list = leer_paises()
     if not paises:
         print("No se pudieron cargar los países.")
         return
     
     try:
-        entrada_valida = False
-        pais_a_buscar = ""
+        entrada_valida:bool = False
+        pais_a_buscar:str = ""
         
         while not entrada_valida:
             try:
@@ -120,79 +133,211 @@ def buscar_pais():
 
 # Elección 2: Filtrar países.
 def filtrar_paises():
-    paises = leer_paises()
+    paises:list = leer_paises()
     if not paises:
         print("No se pudieron cargar los países.")
         return
-       
-    print(f"""
-    ⏐⏐≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡⏐⏐
-    ⏐⏐                Filtrar paises               ⏐⏐
-    ⏐⏐≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡⏐⏐     
-    ⏐⏐           Seleccione una opción.            ⏐⏐
-    ⏐⏐                                             ⏐⏐ 
-    ⏐⏐          (1) Por Continentes.               ⏐⏐      
-    ⏐⏐          (2) Por Rango de Supercifie.       ⏐⏐      
-    ⏐⏐          (3) Por Rango de Población.        ⏐⏐
-    ⏐⏐          (4) Salir.                         ⏐⏐
-    ⏐⏐                                             ⏐⏐        
-    ⏐⏐≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡⏐⏐         
-        """)
-    #Solicitar al usuario que filtro desea realizar.
-    opcion : str = str(input("Ingrese una opcion: "))
+    opcion = ()
     while opcion != "4":
         try:
-          if opcion == "1":
-              pass
-          elif opcion == "2":
-              pass
-          elif opcion == "3":
-              pass
-          elif opcion == "4":
-              print("Saliendo...")
-          else:
-              print("Opcion invalida, intente nuevamnte!")
+            print(f"""
+            ⏐⏐≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡⏐⏐
+            ⏐⏐                Filtrar paises               ⏐⏐
+            ⏐⏐≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡⏐⏐     
+            ⏐⏐           Seleccione una opción.            ⏐⏐
+            ⏐⏐                                             ⏐⏐ 
+            ⏐⏐          (1) Por continentes.               ⏐⏐      
+            ⏐⏐          (2) Por rango de supercifie.       ⏐⏐      
+            ⏐⏐          (3) Por rango de población.        ⏐⏐
+            ⏐⏐          (4) Volver al menú principal.      ⏐⏐
+            ⏐⏐                                             ⏐⏐        
+            ⏐⏐≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡⏐⏐         
+                """)
+            # Solicitar al usuario que filtro desea realizar.
+            opcion:str = str(input("Usted seleccionó la opción: "))
+            
+            # Filtrado por continentes.  
+            if opcion == "1":
+                try:
+                    entrada_valida:bool = False
+                    continente_a_buscar = ()
+            
+                    while not entrada_valida:
+                        try:
+                            print(f"""
+                            ------------------------------------      
+                                        Continentes
+                            ------------------------------------
+                                    Elija un continente.
+                                    
+                                        (1) África.
+                                        (2) América.
+                                        (3) Asia.
+                                        (4) Europa.
+                                        (5) Oceanía.
+                                        (6) Antártida.
+                            ------------------------------------            
+                                """)
+                            continente_a_buscar = int(input("Usted seleccionó la opción: "))
+                            if 1 <= continente_a_buscar <= 6:
+                                entrada_valida = True
+                            else:
+                                print("Opción inválida. Por favor, intente de nuevo.")       
+                        except ValueError as e:
+                            print(f"Error de valor ingresado. Por favor, ingrese un número válido.") 
+                            print(" ")
+                        except KeyboardInterrupt:
+                            print("Búsqueda cancelada por el usuario.")
+                            return
+                        
+                    continente_elegido = elegir_continente(continente_a_buscar)
+                    encontrados = [
+                        continente for continente in paises
+                        if continente["continente"] == continente_elegido
+                    ]
+                    
+                    if len(encontrados) > 0:
+                        print(f"País(es) encontrado(s) en {continente_elegido}:")
+                        print(" ")
+                        for continente in encontrados:
+                            print(f"Nombre: {continente['nombre']}")
+                            print(f"Población: {continente['poblacion']}")
+                            print(f"Área: {continente['area_km2']} km²")
+                            print(f"Continente: {continente['continente']}")
+                            print("-" * 40)            
+                except Exception as e:
+                    print(f"Error inesperado: {str(e)}")
+            # Filtrado por rango de superficie.          
+            elif opcion == "2":
+                try:
+                    entrada_valida:bool = False
+                    minimo:float = ()
+                    maximo:float = ()
+                    while not entrada_valida:
+                        try:
+                           minimo:float = float(input("Ingrese el minimo del rango de superficie:"))
+                           if minimo <= 0:
+                               print("Error. No se pueden ingresar numeros negativos o cero.")
+                               print(" ")    
+                           else:
+                               maximo:float = float(input("Ingrese el máximo del rango de superficie:"))
+                               if maximo <= 0 or maximo <= minimo:
+                                   print("Error. No se pueden ingresar numeros negativos, cero, o que el maximo sea menor que el minimo.")
+                                   print(" ")
+                               else:                                      
+                                   entrada_valida = True  
+                        except ValueError as e:
+                            print(f"Error de valor ingresado. Por favor, ingrese un número válido.") 
+                            print(" ")
+                        except KeyboardInterrupt:
+                            print("Búsqueda cancelada por el usuario.")
+                            return
+                        
+                    encontrados = [
+                        area for area in paises
+                        if float(area["area_km2"]) >= minimo and float(area["area_km2"]) <= maximo
+                    ]
+                    
+                    if len(encontrados) > 0:
+                        print(f"País(es) con un área entre {minimo} y {maximo}:")
+                        print(" ")
+                        for area in encontrados:
+                            print(f"Nombre: {area['nombre']}")
+                            print(f"Población: {area['poblacion']}")
+                            print(f"Área: {area['area_km2']} km²")
+                            print(f"Continente: {area['continente']}")
+                            print("-" * 40)            
+                except Exception as e:
+                    print(f"Error inesperado: {str(e)}")
+            # Filtrado por rango de población.
+            elif opcion == "3":
+                try:
+                    entrada_valida:bool = False
+                    minimo:int = ()
+                    maximo:int = ()
+                    while not entrada_valida:
+                        try:
+                           minimo:int = int(input("Ingrese el minimo del rango de población:"))
+                           if minimo <= 0:
+                               print("Error. No se pueden ingresar numeros negativos o cero.")
+                               print(" ")    
+                           else:
+                               maximo:int = int(input("Ingrese el máximo del rango de población:"))
+                               if maximo <= 0 or maximo <= minimo:
+                                   print("Error. No se pueden ingresar numeros negativos, cero, o que el maximo sea menor que el minimo.")
+                                   print(" ")
+                               else:                                      
+                                   entrada_valida = True  
+                        except ValueError as e:
+                            print(f"Error de valor ingresado. Por favor, ingrese un número válido.") 
+                            print(" ")
+                        except KeyboardInterrupt:
+                            print("Búsqueda cancelada por el usuario.")
+                            return
+                        
+                    encontrados = [
+                        poblacion for poblacion in paises
+                        if int(poblacion["poblacion"]) >= minimo and int(poblacion["poblacion"]) <= maximo
+                    ]
+                    
+                    if len(encontrados) > 0:
+                        print(f"País(es) con una poblacion entre {minimo} y {maximo}:")
+                        print(" ")
+                        for poblacion in encontrados:
+                            print(f"Nombre: {poblacion['nombre']}")
+                            print(f"Población: {poblacion['poblacion']}")
+                            print(f"Área: {poblacion['area_km2']} km²")
+                            print(f"Continente: {poblacion['continente']}")
+                            print("-" * 40)            
+                except Exception as e:
+                    print(f"Error inesperado: {str(e)}")
+            # Salir.
+            elif opcion == "4":
+                print("Volviendo al menú principal...")
+            else:
+                print("Opción inválida. Port favor, intente de nuevo.")
         except KeyboardInterrupt:
             print(f"Programa terminado por el usuario.")
         except ValueError as e:
             print(f"Error de valor ingresado. Por favor, ingrese un número válido.")  
-        
+            
 # Elección 3: Ordenar países.
 def ordenar_paises():
     paises = leer_paises()
     if not paises:
         print("No se pudieron cargar los países.")
         return
-    print(f"""
-    ⏐⏐≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡⏐⏐
-    ⏐⏐                Ordenar paises               ⏐⏐
-    ⏐⏐≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡⏐⏐     
-    ⏐⏐           Seleccione una opción.            ⏐⏐
-    ⏐⏐                                             ⏐⏐ 
-    ⏐⏐          (1) Por Nombre.                    ⏐⏐      
-    ⏐⏐          (2) Por Poblacion.                 ⏐⏐      
-    ⏐⏐          (3) Por Superfice Ascendente.      ⏐⏐
-    ⏐⏐          (4) Por Superfice Desencente.      ⏐⏐
-    ⏐⏐          (5) Salir.                         ⏐⏐
-    ⏐⏐                                             ⏐⏐        
-    ⏐⏐≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡⏐⏐         
-        """)
-    #Solicitar al usuario que como ordenar a los paises.
-    opcion : str = str(input("Ingrese una opcion: "))
+    opcion = ()
     while opcion != "5":
         try:
-          if opcion == "1":
-              pass
-          elif opcion == "2":
-              pass
-          elif opcion == "3":
-              pass
-          elif opcion == "4":
-              pass
-          elif opcion == "5":
-              print("Saliendo...")
-          else:
-              print("Opcion invalida, intente nuevamnte!")
+            print(f"""
+            ⏐⏐≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡⏐⏐
+            ⏐⏐                Ordenar paises               ⏐⏐
+            ⏐⏐≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡⏐⏐     
+            ⏐⏐           Seleccione una opción.            ⏐⏐
+            ⏐⏐                                             ⏐⏐ 
+            ⏐⏐          (1) Por Nombre.                    ⏐⏐      
+            ⏐⏐          (2) Por Poblacion.                 ⏐⏐      
+            ⏐⏐          (3) Por Superfice Ascendente.      ⏐⏐
+            ⏐⏐          (4) Por Superfice Desencente.      ⏐⏐
+            ⏐⏐          (5) Volver al menú principal.      ⏐⏐
+            ⏐⏐                                             ⏐⏐        
+            ⏐⏐≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡⏐⏐         
+                """)
+            # Solicitar al usuario que como ordenar a los paises.
+            opcion : str = str(input("Ingrese una opcion: "))
+            if opcion == "1":
+                pass
+            elif opcion == "2":
+                pass
+            elif opcion == "3":
+                pass
+            elif opcion == "4":
+                pass
+            elif opcion == "5":
+                print("Volviendo al menú principal...")
+            else:
+                print("Opcion invalida, intente nuevamnte!")
         except KeyboardInterrupt:
             print(f"Programa terminado por el usuario.")
         except ValueError as e:
@@ -224,46 +369,18 @@ def estadisticas():
             # Solicitar al usuario que estadistica desea ver.
             opcion : str = str(input("Ingrese una opcion: "))  
             if opcion == "1":
-                print("op 1") 
-                #Convertimos las poblaciones a número enteros para mejor facilidad al comparar dentro de una lista de tuplas
-                poblaciones = [(int(p['poblacion']),p['nombre']) for p in paises]
-                mayor_poblacion = max(poblaciones)
-                menor_poblacion = min(poblaciones)
-                print(f"País con Mayor Población: {mayor_poblacion} ")
-                print(f"País con Menor Población: {menor_poblacion} ")
+                pass
             elif opcion == "2":
-                print("OP2")
-                #Calcular promedio de Población
-                total_poblacion = sum(int(p['poblacion']) for p in paises)
-                promedio_poblacion = total_poblacion / len(paises)
-                print(f"Promedio de Población: {promedio_poblacion:.2f}")
+                pass
             elif opcion == "3":
-                print("OP3")
-                #Calcular promedio de Superficie
-                total_superficie = sum(float(p['area_km2']) for p in paises)
-                promedio_superficie = total_superficie / len(paises)
-                print(f"Promedio de Superficie: {promedio_superficie:.2f}")
+                pass
             elif opcion == "4":
-                print("OP4")
-                #Calcular cuantos paises hay por continente
-                cantidad_continentes = []
-                for pais in paises:
-                    continente = pais['continente']
-                    cantidad_continentes.append(continente)
-                conteo = {}
-                for continente in cantidad_continentes:
-                    if continente in conteo:
-                        conteo[continente] += 1
-                    else:
-                        conteo[continente] = 1
-                print("Cantidad de Países por Continente:")
-                for continente, cantidad in conteo.items():
-                    print(f"{continente}: {cantidad}")
+                pass
             elif opcion == "5":
                 print("Volviendo al menú principal...")
             else:
                 print("Opcion invalida, intente nuevamnte!")
         except KeyboardInterrupt:
-            print("Programa terminado por el usuario.")
+            print(f"Programa terminado por el usuario.")
         except ValueError as e:
-            print(f"{e} Error de valor ingresado. Por favor, ingrese un número válido.") 
+            print(f"Error de valor ingresado. Por favor, ingrese un número válido.")  
